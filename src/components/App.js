4e53ae4afd0439148419688605070573065da425
Alex Stiles <alinnstiles@gmail.com>
2024-04-10 15:39:24 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,38 @@
 import React from "react";
 
-function NavBar() {
-  return (
+function NavBar(props) {
+ return (
     <nav>
       <a href="#home">I'm a link!</a>
+      <h1>{props.comment}</h1>
     </nav>
-  );
+ );
 }
 
-function Home() {
-  return (
+function Home(props) {
+ return (
     <div id="home">
       <h1>Home</h1>
     </div>
-  );
+ );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+ return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+ );
+}
 
 function App() {
-  return (
+ return (
     <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <NavBar comment="I'm a link!" />
+      <Home />
+      <About />
     </div>
-  );
+ );
 }
 
 export default App;
